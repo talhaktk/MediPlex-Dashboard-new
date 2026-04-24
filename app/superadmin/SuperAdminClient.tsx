9fb7bbb1d619fb31c09e7255a56dfdb49e402fe7
Talha <eapple@Es-MacBook-Pro-2.local>
2026-04-24 16:20:37 +0500

    fix: add city/province to Org type

--- app/superadmin/SuperAdminClient.tsx
+++ app/superadmin/SuperAdminClient.tsx
@@ -14,6 +14,7 @@ import toast from 'react-hot-toast';
 type Org = {
   id: string; name: string; owner_name: string;
   email: string; phone: string; status: string; created_at: string;
+  city?: string; province?: string; country?: string;
 };
 
 type Clinic = {
